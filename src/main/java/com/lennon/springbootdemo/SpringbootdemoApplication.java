62d03a594fa02719e78ab86a50ebde702d2b806c
Cube2020 <32156937+Cube2020@users.noreply.github.com>
2020-07-23 16:21:40 +0800

    Revert "pppppp"

--- src/main/java/com/lennon/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/lennon/springbootdemo/SpringbootdemoApplication.java
@@ -15,7 +15,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootdemoApplication {
 
     public static void main(String[] args) {
-        System.out.println("pppppp");
         SpringApplication.run(SpringbootdemoApplication.class, args);
     }
 
